6a280a56d89dbf0b9826985697536a7432c03697
VladyslavRiabets <vladislav.ryabets@apriorit.com>
2024-12-07 02:05:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,34 +92,25 @@ resource "azurerm_network_interface" "nic" {
 
 # Virtual Machine
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "jenkins-ubuntu-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_B1s"
-  admin_username        = "azureuser"
-  admin_password        = "Sempertemp12345!"
+  name                    = "jenkins-ubuntu-vm"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  network_interface_ids   = [azurerm_network_interface.nic.id]
+  size                    = "Standard_B1s"
+  disable_password_authentication = false
+  admin_username          = "azureuser"
+  admin_password          = "Sempertemp1234!"
 
-  storage_os_disk {
-    name              = "osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  os_disk {
+    caching                = "ReadWrite"
+    storage_account_type   = "Standard_LRS"
   }
 
-  storage_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "jenkinsvm"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
   }
 }
 
