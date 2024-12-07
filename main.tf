7f09690f49633afba5088b1b057c33a9740c783f
VladyslavRiabets <vladislav.ryabets@apriorit.com>
2024-12-07 02:10:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,38 +92,38 @@ resource "azurerm_network_interface" "nic" {
 
 # Virtual Machine
 resource "azurerm_virtual_machine" "vm" {
-  name                    = "jenkins-ubuntu-vm"
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  network_interface_ids   = [azurerm_network_interface.nic.id]
-  size                    = "Standard_B1s"
-  disable_password_authentication = false
-  admin_username          = "azureuser"
-  admin_password          = "Sempertemp1234!"
+  name                  = "jenkins-ubuntu-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
 
-  os_disk {
-    caching                = "ReadWrite"
-    storage_account_type   = "Standard_LRS"
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
 
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
+
+  os_profile {
+    computer_name  = "jenkinsvm"
+    admin_username = "azureuser"
+    admin_password = "Sempertemp12345!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
 
-# Output Public IP, username  and password of the VM
+# Output Public IP of the VM
 output "vm_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
-
-output "vm_username" {
-  value = azurerm_virtual_machine.vm.admin_username
-}
-
-output "vm_password" {
-  value = azurerm_virtual_machine.vm.admin_password
-}
-
